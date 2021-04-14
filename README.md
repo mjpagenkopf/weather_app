a1c7b6b9f535d449fd4b5566b1ded7a0aa4e389e
mjpagenkopf <78058404+mjpagenkopf@users.noreply.github.com>
2021-04-14 17:28:26 -0400

    finalizing my weather app and pushing to repo. Includes changes to html, css and js

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # weather_app
+
+---
+
+## Summary of Criteria
+
+Assignment was to create a weather application that will:
+
+* use OneWeatherMap API to create a weather dashboard that provides results to user search using form inputs 
+
+* use fetch to get weather information response.
+
+* include city name, date, weather icon, temperature, wind speed and UV index in results
+
+* provide 5 day weather forecast for location.
+
+* create buttons for history searches that user can use to call results again.
+
+---
+
+## Summary of Experience
+
+- The successes of my efforts were getting the dashboard to fetch the API information successfully from the user input and display different information based on multiple fetch responses, including using Moment as an additional API. I was also able to successfully write most of the html using javascript, jquery and template literals. 
+
+- Problems I experienced included many - most of this assignment took me a long time to get to work. I also was unable to finish the part where the 5 day forecast would replace with each search. I will need to fix that. 
+
+
+# Links to active site and repo below:
+
+[Weather Dashboard Active Link](https://mjpagenkopf.github.io/work_day_scheduler/)
+
+[Weather Dashboard Repository Link](https://github.com/mjpagenkopf/work_day_scheduler)
+
+
+![Screenshot of Work Day Scheduler](./assets/screenshot_work_day_scheduler.png)
--- assets/script.js
+++ assets/script.js
@@ -2,20 +2,17 @@ const KEY = "&appid=84e804a2f27fd4b395f985c7c7fd8ead";
 const URL = "https://api.openweathermap.org/data/2.5/weather?q="
 const URLtwo = "https://api.openweathermap.org/data/2.5/uvi?lat="
 const URLthree = "&lon=";
-const URLfour = "https://api.openweathermap.org/data/2.5/forecast?q=";
+const URLfour = "https://api.openweathermap.org/data/2.5/onecall?lat="; //"https://api.openweathermap.org/data/2.5/forecast?q="
 const nameInputEl = document.getElementById("form-input");
 const submitButton = document.getElementById("btn-submit");
-const weatherBox = document.getElementsByClassName('weather-box')[0];
-const weatherContent = document.getElementsByClassName('weather-content')[0];
-const weatherOverview = document.getElementsByClassName('weather-overview')[0];
-const weatherTemp = document.getElementsByClassName('weather-temp')[0];
+
 const history = document.querySelector(".city-buttons");
 historyCountSpan = document.querySelector("#history-count")
 
 let cityLocalStorageObject = {}; 
 let citiesArray = [];
-let longitude;
-let latitude;
+var longitude;
+var latitude;
 
 
 
@@ -26,47 +23,36 @@ const addCity = (ev) => {
     city: nameInputEl.value,
     }
 
-      if (cityLocalStorageObject === "") {
+      if (cityLocalStorageObject.city === "") { /* added .city */
       return;
       }
         //citiesArray has new object KEY:VALUE added to end of array
         citiesArray.push(cityLocalStorageObject);
-        document.querySelector("form").reset();
+        document.querySelector('form').reset();
         console.log(citiesArray);
 
-      localStorage.setItem("cityList", JSON.stringify(citiesArray));
+      localStorage.setItem('cityList', JSON.stringify(citiesArray));
   
         let cityName = cityLocalStorageObject.city;
         console.log(cityName);
-
-        fetch(URL + cityName + KEY)
+        searchWeather(cityName);
+}
+//function searchWeather fetches main City API
+function searchWeather(searchValue) {
+        fetch(URL + searchValue + KEY)
         .then(function (response) {
         return response.json()
         })
         .then(function (data) {
         console.log(data)
+        //drawWeather writes out the response
         drawWeather(data);
+        forecastData(data);
         })
         .catch(function() {
         //catch errors
         });
-
-        // fetch(URLfour + cityName + KEY)
-        // .then(function (response) {
-        //   return response.json()
-        // })
-        // .then(function (dataThree) {
-        //   console.log(dataThree)
-        //   forecast(dataThree);
-        // })
-        // .catch(function() {
-
-        // });
-
-    
-  // weatherContainer();
   renderCitiesList();
-  
 }
 
 function renderCitiesList() {
@@ -76,38 +62,39 @@ function renderCitiesList() {
     for (let i = 0; i < citiesArray.length; i++) {
       city = citiesArray[i];
 
-      // let li = document.createElement("li");
-      // li.textContent = citiesArray[i].city;
-      // li.setAttribute("data-index", i);
-      // console.log(li)
-
       let button = document.createElement("button");
       button.textContent = citiesArray[i].city;
       button.setAttribute("id", "btn-submit");
       button.setAttribute("data-index", i);
-
+      button.addEventListener('click', function(event) {
+        event.preventDefault();
+        searchWeather(event.target.textContent);
+      })
     // li.appendChild(button);
     history.appendChild(button);
   }
-
 }
-
 
 function drawWeather(d) {
   // document.getElementById('date').innerHTML = (moment().format("LLLL"));
-	let fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
 	
-  document.getElementById('location').innerHTML = d.name,
-    
+  document.getElementById('location').innerHTML = d.name;
+  let today = $('<p>')
+    // .addClass('column')
+    .attr('id', 'today')
+    .text(moment().add(0, 'days').calendar("LL"));
+    $('#location').append(today)
+  
+  document.getElementById('icon-image').setAttribute("src", "http://openweathermap.org/img/wn/" + d.weather[0].icon + "@2x.png"),  
+
 	document.getElementById('description').innerHTML = d.weather[0].description;
-  document.getElementsByClassName('icon-image').innerHTML = d.weather[1].icon;
+  
 	document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
-    // document.getElementById('clouds').innerHTML = d.clouds.all;
-	  
+    
   document.getElementById('humidity').innerHTML = d.main.humidity;
   document.getElementById('wind-speed').innerHTML = d.wind.speed;
 
-    // document.getElementById('uv-index').innerHTML = d.name;
     longitude = document.getElementById('lon').innerHTML = d.coord.lon;
     latitude = document.getElementById('lat').innerHTML = d.coord.lat;
 
@@ -118,6 +105,7 @@ function drawWeather(d) {
       .then(function (dataTwo) {
       console.log(dataTwo)
       uvIndex(dataTwo);
+      forecastData();
       })
       .catch(function() {
       });
@@ -128,15 +116,110 @@ function uvIndex(dataTwo) {
   document.getElementById('uv-index').removeAttribute('style');
 }
 
-// function forecast(dataThree) {
-//   document.getElementById('forecast').innerHTML = dataThree[0].weather;
+
+function forecastData(f) {
+  longitude = f.coord.lon;
+  latitude = f.coord.lat;
+
+  fetch(URLfour + latitude + URLthree + longitude + '&exclude={current, minutely,hourly,alerts}' + KEY)  //fetch(URLfour + searchValue + KEY)
+  .then(function (response) {
+  if (!response.ok) {
+  throw Error("ERROR");
+  }
+  return response.json()
+  })
+  .then(dataThree => {
+  console.log(dataThree.daily); //[0].weather[0]
+  
+  const forecastList = dataThree.daily
+  var dayBox = '';
+  
+  for(var i = 0; i < 5; i++) {
+    console.log(`${forecastList[i].temp}`)
+    // forecastList.forEach(function(daily, i) {
+    // $(`#day-${i}`).val(daily.weather[i])
+    // console.log(forecastList);
+    // const weatherList
+    //  })
+    var fahrenheit2 = Math.round(((parseFloat(`${forecastList[i].temp.day}`)-273.15)*1.8)+32);
+    console.log(fahrenheit2);
+    // weatherList.forEach(function(daily, i) {
+    dayBox = $('<div>')
+    .addClass('column card')
+    .attr('id', `day-${i}`)
+    // .text(`${forecastList[i].weather[0].main}`)
+    let dateBox = $('<p>')
+    .addClass('column temp-size')
+    .attr('id', `date-${i}`)
+    .text(moment().add(`${i}`, 'days').calendar("LL"));
+    let iconP = $('<p>')
+
+    let iconBox = $('<img>')
+    
+    .attr('id', `icon-${i}`)
+    .attr("src", "http://openweathermap.org/img/wn/" + forecastList[i].weather[0].icon + "@2x.png")
+    var tempBox = $('<p>')
+    .addClass('column temp-size')
+    .attr('id', `temp-${i}`)
+    .text("Temperature: " + fahrenheit2)
+    var windBox = $('<p>')
+    .addClass('column wind-size')
+    .attr('id', `wind-${i}`)
+    .text(`Wind Speed: ${forecastList[i].wind_speed} mph`)
+    // $('#forecast-header').prepend('#forecast')
+    $('#forecast').append(dayBox)
+    dayBox.append(dateBox);
+    dateBox.append(tempBox);
+    tempBox.append(iconP);
+    iconP.append(iconBox);
+    iconP.after(windBox);
+    
+    
+    // $(`day-${i}`)
+    // let tempBox = $('<p>')
+        }
+      
+     }
+    )
+   }
+   
+
+//can i make forecastList = the object of datathree
+
+//gets forecast with response from earlier
+// function forecast(dataThree) { //only needs the object dataThree once. inside key array must be looped
+// const forecastList = dataThree.daily;
+// console.log(forecastList);
+// };
+
+// forecastList.forEach(function(daily, i) {
+
+ 
+//   for(let i = 0; i < 5; i++) {
+  
+  // const forecastBox = dataThree.daily[i].map(d => {
+  //   return `<div class="card">
+  //   <p>${d.dt.val()}</p>
+  //   <p>${d.temp.day.val()}</p>
+  //   </div>
+  //   `; 
+  // })
+  // .join('');
+  // console.log(forecastBox);
+  
+
+  // document
+  //   .querySelector('#forecast-header')
+  //   .insertAdjacentElement('afterbegin', forecastBox);
+  
+
+
+// }
+    
+// }
+   
 // }
 
-// forecast.forEach(day => {
-//   let date = new Date(day.dt * 1000);
-
-
-// })
 
 submitButton.addEventListener("click", addCity);
 
@@ -145,125 +228,47 @@ submitButton.addEventListener("click", addCity);
 
 
 
-//ATTEMPTING TO CREATE HISTORY RELOAD SECTION
-// const cityNameReturn = JSON.parse(localStorage.getItem('cityList'));
-// console.log(cityNameReturn)
-// cityNameValue = cityNameReturn.citiesArray[i].value;
-// console.log(cityNameValue);
 
 
-// function weatherContainer ()
-// fetch(URL + updater + KEY)
-// .then(function (response) {
+
+
+
+
+
+
+
+
+//   fetch(URLfour + latitude + URLthree + longitude + '&exclude={current, minutely,hourly,alerts}' + KEY)  //fetch(URLfour + searchValue + KEY)
+//   .then(function (response) {
+//   if (!response.ok) {
+//   throw Error("ERROR");
+//   }
 //   return response.json()
-// })
-// .then(function (res) {
-//   console.log(res)
-// })
+//   })
+//   .then(function (dataThree) {
+//   console.log(dataThree)
+//   forecast(dataThree);
+//   })
+//   .catch(function() {
+//   }); 
+
+
+  // console.log(forecastBox);
+//   )
+//   document.getElementById('forecast-header').insertAdjacentHTML('afterend', '<div></div>');
+
+  // for(let i = 0; i < 5; i++) {
+  //   let forecastList = dataThree.daily[i];
 
 
 
 
 
-// let city = nameInputEl.value.trim();
-
-// let getUserSearch = function (city) {
-//     let url = "https://api.openweathermap.org/data/2.5/weather?q=" + nameInputEl + "&appid=84e804a2f27fd4b395f985c7c7fd8ead";
-    
-    
-//      fetch(url)
-//         .then(function (response) {
-//           if (response.ok) {
-//             console.log(response);
-//             response.json().then(function (data) {
-//               console.log(data);
-//               displaySearchResults(data, user);
-//             });
-//           } else {
-//             alert('Error: ' + response.statusText);
-//           }
-//         })
-//         .catch(function (error) {
-//           alert('Unable to connect');
-//         });
 
 
-// let formSubmitHandler = function (event) {
-//   event.preventDefault();
-
-//   let cityName = nameInputEl.value.trim();
-
-//   if (cityName) {
-//     getUserSearch(cityName);
-//     resultsContainerEl.textContent = '';
-//     nameInputEl.value = '';
-//   } else {
-//     alert('Please enter a city');
-//   }
-// };
-
-// let buttonClickHandler = function (event) {
-//   let city = event.target.getAttribute('data-city');
-
-//   if (city) {
-//     getFeaturedCities(city);
-
-//     resultsContainerEl.textContent = '';
-//   }
-// };
-
-
-
-//     function getFeaturedCities(city) {
-//         var apiUrl = '' + city + '';
-
-//         fetch(apiUrl).then(function (response) {
-//             if (response.ok) {
-//                 response.json().then(function (data) {
-//                 displaySearchResults(data.items, city);
-//                 });
-//             } else {
-//                 alert('Error: ' + response.statusText);
-//         }
-//         });
-//     }
-
-// var displaySearchResults = function (repos, searchTerm) {
-//   if (repos.length === 0) {
-//     repoContainerEl.textContent = 'No repositories found.';
-//     return;
-//   }
-
-//   repoSearchTerm.textContent = searchTerm;
-
-//   for (var i = 0; i < repos.length; i++) {
-//     var repoName = repos[i].owner.login + '/' + repos[i].name;
-
-//     var repoEl = document.createElement('a');
-//     repoEl.classList = 'list-item flex-row justify-space-between align-center';
-//     repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
-
-//     var titleEl = document.createElement('span');
-//     titleEl.textContent = repoName;
-
-//     repoEl.appendChild(titleEl);
-
-//     var statusEl = document.createElement('span');
-//     statusEl.classList = 'flex-row align-center';
-
-//     if (repos[i].open_issues_count > 0) {
-//       statusEl.innerHTML =
-//         "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-//     } else {
-//       statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-//     }
-
-//     repoEl.appendChild(statusEl);
-
-//     repoContainerEl.appendChild(repoEl);
-//    }
-//  };
-
-// userFormEl.addEventListener('submit', formSubmitHandler);
-// cityButtonsEl.addEventListener('click', buttonClickHandler);
-// }
+ //   <h5 class="card-title">Card title</h5>
+    //   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    //   <a href="#" class="btn btn-primary">Go somewhere</a>)
+    // </div>
+    // </div>
+    // `)
